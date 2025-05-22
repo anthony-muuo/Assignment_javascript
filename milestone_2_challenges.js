049da83df5a3621a83cef401fba78f77d56fa206
muuo <anthonymuuo92@gmail.com>
2025-05-22 20:59:21 +0300

    completed challenge 5: count key properties in an object

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -51,4 +51,10 @@ function findLongestWord(array) {
   }
   return longest;
 }
-console.log(findLongestWord(["apple", "banana", "pear", "grapefruit"]));
+// console.log(findLongestWord(["apple", "banana", "pear", "grapefruit"]));
+
+// Challenge 5: count properties
+function countProperties(object) {
+  return Object.keys(object).length;
+}
+console.log(countProperties({ name: "Alice", age: 25, city: "Paris" }));
