c0da80739dc9c1ae966825c63d7c3c9267674dbb
muuo <anthonymuuo92@gmail.com>
2025-05-22 09:35:03 +0300

    completed solution one: sum of positives number

---
+++ milestone_2_challenges.js
@@ -0,0 +1,12 @@
+// Challenges One: sum of positive numbers
+
+function sumOfPositives(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) {
+      sum += array[i];
+    }
+  }
+  return sum;
+}
+console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
