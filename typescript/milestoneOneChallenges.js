eb968c7386a1dbce15abe3406d724ef5b8709cd1
muuo <anthonymuuo92@gmail.com>
2025-06-01 12:04:27 +0300

    converted challenge one to ts

---
+++ typescript/milestoneOneChallenges.js
@@ -0,0 +1,5 @@
+//challenge one : sum of two numbers
+function addition(num1, num2) {
+    return num1 + num2;
+}
+console.log(addition(3, 4));
---
+++ typescript/milestoneOneChallenges.ts
@@ -0,0 +1,7 @@
+//challenge one : sum of two numbers
+
+function addition(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(addition(3, 4));
